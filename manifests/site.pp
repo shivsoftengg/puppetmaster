1df059673580bcb3406dc567c0eac04dbcc77fb2
Shiv <shiv@testnode.shiv.com>
2013-10-12 20:20:36 +0100

    Added a code to create file name /tmp/node1

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,3 +3,8 @@ file { '/tmp/node2':
 	ensure => file,
 	content => 'And thats how we do it',
 }
+
+file {'/tmp/node1':
+	ensure => file,
+	content => 'A file name node1, testing my rakefile autodeploy',
+}
